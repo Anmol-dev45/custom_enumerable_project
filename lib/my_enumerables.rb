dbcdf5c5d5d788de5c87b2f804f9a3345fcdec18
Anmol-dev45 <basnetanmol2020@gmail.com>
2024-07-28 23:08:46 +0545

    Custom methods created

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,4 +8,70 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    for elm in self do
+      yield elm
+    end
+    self  
+  end 
+
+  def my_any?
+    flag = false
+    self.my_each do |elm|
+      if yield(elm) 
+        flag = true
+        break
+      end  
+    end
+    flag  
+  end
+
+  def my_count
+    return self.length unless block_given?
+    count = 0
+    self.my_each do |elm|
+      count += 1 if yield(elm) 
+    end
+    count
+  end
+
+  def my_each_with_index
+    i = 0
+    while i < self.length
+      yield(self[i], i)
+      i += 1
+    end
+    self 
+  end
+
+  def my_inject acc = 0
+    self.my_each do |cur|
+      acc = yield(acc,cur)
+    end
+    acc
+  end  
+
+  def my_map 
+    arr = []
+    self.each { |elm| arr << yield(elm) }
+    arr
+  end
+
+  def my_none?
+    flag = true
+    self.my_each do |elm|
+      if yield(elm)
+        flag = false
+        break
+      end
+    end
+    flag
+  end
+
+  def my_select
+    arr = []
+    self.each { |elm| arr << elm if yield(elm) }
+    arr
+  end
+
 end
